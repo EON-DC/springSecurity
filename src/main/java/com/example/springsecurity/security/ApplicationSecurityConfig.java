d82eee12d76433bf6563f20b00e23765dcecff72
EON-DC <mun0392@gmail.com>
2022-11-12 21:14:44 +0900

    Spring Boot Security follow up Amigoscode on Youtube

--- src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.springsecurity.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,10 +14,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.example.springsecurity.security.ApplicationUserPermission.*;
 import static com.example.springsecurity.security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     /**
      * Basic Authentication 은 로그아웃 불가능, 간단한 리퀘스트에만 인가함
@@ -30,8 +34,23 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
      * ► Argon2PasswordEncoder
      * ► DelegatingPasswordEncoder
      * ► SCryptPasswordEncoder 등
-     * <p>
-     * 선택
+     *
+     * 그 다음으로 유저에 대한 ROLE 과 Permission을 구분해주는데, enum type으로 정의하였다.
+     * PERMISSION 에 대해선, COURSE WRITE, COURSE READ, STUDENT WRTIE, STUDENT READ로 구분하였다.
+     *      *
+     * Role 에 대해 STUDENT, ADMIN, ADMINTRAINEE 로 구분하였고
+     * 구글 구아바를 이용해 hashSet으로 PERMISSION 을 등록하였다.
+     *
+     * 이후 UserDetails에 ROLE을 등록하였고, configure에서는 antMatchers를 통해 permission 없이 들어갈 수 있는 경로
+     * 각 Authority에 가능한 경로+HttpMethod 에 대해 지정하였다.
+     * ✱ 참고로 'antMatchers' 에 대한 어원은 Apache Ant Home 과 Spring AntPathMatcher 에서 사용한 방식으로 비롯됨
+     *
+     * UserDetails.builder 의 Role의 역할은 입력받은 문자열에 대해 "Role_"을 붙이는 역할을 함.
+     * roles은 GrantedAuthority 를 참조하는데 이를 바탕으로 custom 할 수 있다.
+     *
+     * antMatchers의 순서는 위애서부터 걸러지므로, GET 같은 경우엔 위로 올려 걸러지지 않도록 한다.
+     *  Controller에서 @PreAuthorize 애노테이션을 사용할수 있으며, antMathcers를 대체하는 역할을 한다.
+     *  annotation을 사용할 경우 @EnableGlobalMethodSecurity(prePostEnabled = true) 를 입력해줘야 인식한다.
      */
 
     private final PasswordEncoder passwordEncoder;
@@ -44,9 +63,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable() //TODO : 나중에 배울 예정
                 .authorizeRequests()
-                .antMatchers("/", "index", "/css/**", "/js/**").permitAll()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
+//                .antMatchers("/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name()) // 기본 GET
+//                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -59,17 +83,26 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails parkUser = User.builder()
                 .username("park")
                 .password(passwordEncoder.encode("1234"))
-                .roles(STUDENT.name()) // ROLE_STUDENT
+//                .roles(STUDENT.name()) // ROLE_STUDENT
+                .authorities(STUDENT.getGrantedAuthorities())
                 .build();
 
         UserDetails lindaUser = User.builder()
                 .username("linda")
                 .password(passwordEncoder.encode("12345"))
-                .roles(ADMIN.name()) // ROLE_ADMIN
+//                .roles(ADMIN.name()) // ROLE_ADMIN
+                .authorities(ADMIN.getGrantedAuthorities())
+                .build();
+
+        UserDetails tomUser = User.builder()
+                .username("tom")
+                .password(passwordEncoder.encode("12345"))
+//                .roles(ADMINTRAINEE.name()) // ROLE_ADMIN
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
                 .build();
 
         return new InMemoryUserDetailsManager(
-                parkUser, lindaUser
+                parkUser, lindaUser, tomUser
         );
     }
 }
--- src/main/java/com/example/springsecurity/security/ApplicationUserRole.java
+++ src/main/java/com/example/springsecurity/security/ApplicationUserRole.java
@@ -1,13 +1,17 @@
 package com.example.springsecurity.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.example.springsecurity.security.ApplicationUserPermission.*;
 
 public enum ApplicationUserRole {
     STUDENT(Sets.newHashSet()),
+    ADMINTRAINEE(Sets.newHashSet(COURSE_READ, STUDENT_READ)),
     ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_READ, STUDENT_WRITE));
 
     private final Set<ApplicationUserPermission> permissions;
@@ -17,6 +21,14 @@ public enum ApplicationUserRole {
     }
 
     public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities(){
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
         return permissions;
     }
 }
